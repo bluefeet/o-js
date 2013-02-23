98e2d6b9c968e179a977415581949d5a868cab1b
Aran Deltac <bluefeet@gmail.com>
2013-02-23 09:39:02 -0800

    First commit.

---
+++ o.js
@@ -0,0 +1,129 @@
+var _ = require('./underscore.js');
+
+(function() {
+    var root = this;
+    var previous = root.o;
+
+    var o = {};
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = o;
+        }
+        exports.o = o;
+    } else {
+        root.o = o;
+    }
+
+    o.noConflict = function () {
+        root.o = previous;
+        return o;
+    };
+
+    o.ucfirst = function (str) {
+        return str.charAt(0).toUpperCase() + str.substr(1);
+    };
+
+    o.before = function (original, func) {
+        return function () {
+            func.call( this );
+            return original.apply( this, arguments );
+        };
+    };
+
+    o.after = function (original, func) {
+        return function () {
+            var ret = original.apply( this, arguments );
+            func.call( this );
+            return ret;
+        };
+    };
+
+    o.around = function (original, func) {
+        return function () {
+            var self = this;
+            var args = Array.prototype.slice.call(arguments);
+            var wrapper = function () {
+                original.apply( self, arguments );
+            };
+            args.unshift( wrapper );
+            func.apply( self, args );
+        };
+    };
+
+    o.extend = function (constructor, parent) {
+        var child = o.around(
+            parent,
+            constructor
+        );
+
+        var proto = _.clone( parent.prototype || {} );
+        _.extend(
+            proto,
+            (constructor.prototype || {})
+        );
+
+        child.prototype = proto;
+
+        return child;
+    };
+
+    o.predicate = function (key) {
+        return function () {
+            return _.has( this, key );
+        };
+    };
+
+    o.clearer = function (key) {
+        return function () {
+            delete this[key];
+        };
+    };
+
+    o.reader = function (key, def) {
+        var writer = def.writer || o.writer( key, def );
+        var predicate = def.predicate || o.predicate( key );
+
+        return function () {
+            if (!predicate.call( this )) {
+                if (def.default !== undefined) {
+                    var value = def.default;
+                    if (typeof value === 'function') { 
+                        value = value.call( this );
+                    }
+
+                    writer.call( this, value );
+                }
+                else if (def.required) {
+                    throw new Error('...');
+                }
+            }
+
+            return this[key];
+        };
+    };
+
+    o.writer = function (key, def) {
+        return function (value) {
+            if (def.option && value === undefined) { value = true }
+            if (def.coerce) { value = def.coerce.call( this, value ) }
+            if (def.isa && !def.isa.call( this, value )) { throw new Error('...') }
+
+            this[key] = value;
+            if (def.chain || def.option) { return this }
+            return value;
+        };
+    };
+
+    o.accessor = function (key, def) {
+        var writer = def.writer || o.writer( key, def );
+        var reader = def.reader || o.reader( key, def );
+
+        return function (value) {
+            if (value !== undefined) {
+                writer.call( this, value );
+            }
+            return reader.call( this );
+        };
+    };
+}).call(this);
