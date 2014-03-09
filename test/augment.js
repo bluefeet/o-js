58b0dabe3d1d987a713abb90ab77483d961d3731
Aran Deltac <bluefeet@gmail.com>
2014-03-09 13:53:12 -0700

    Remove all references to __proto__.

--- test/augment.js
+++ test/augment.js
@@ -15,9 +15,6 @@ test('basic', function (t) {
     t.equal( obj.foo, 'FOO', 'constructor arguments for parent made it through' );
     t.equal( obj.bar, 'BAR', 'constructor arguments for child made it through' );
 
-    t.equal( obj.__proto__, Child.prototype, 'objects prototype came from the child constructor' );
-    t.equal( Child.prototype.__proto__, Parent.prototype, 'childs prototype came from the parent constructor' );
-
     t.ok( obj instanceof Child, 'object is an instance of the child constructor' );
     t.ok( obj instanceof Parent, 'object is an instance of the parent constructor' );
 
@@ -31,17 +28,6 @@ test('basic', function (t) {
     );
     var obj2 = new Child2( 'FOO', 'BAZ' );
     t.equal( obj2.getBaz(), 'BAZ', 'prototype argument was copied' );
-
-    var Child3 = function (parent, foo, zap, mez) { parent(foo); this.zap = zap; this.mez = mez };
-    Child3.prototype.getZap = function () { return this.zap };
-    Child3 = o.augment(
-        Parent,
-        Child3,
-        { getMez: function () { return this.mez } }
-    );
-    var obj3 = new Child3( 'FOO', 'ZAP', 'MEZ' );
-    t.equal( obj3.getZap(), 'ZAP', 'prototype property was copied' );
-    t.equal( obj3.getMez(), 'MEZ', 'constructor prototype was copied' );
 
     t.end();
 });
